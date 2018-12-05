7f7b74d851148607ec81c775f61900a2d785b296
Rafa Roman <33625416+rafaroman18@users.noreply.github.com>
2018-12-05 18:17:27 +0100

    Add files via upload

---
+++ Conjunto/ElitistReplace.m
@@ -0,0 +1,14 @@
+function [Pob_,Ev_] = ElitistReplace(Pob,NewPob,Ev,NewEv)
+
+tempPob = [Pob;NewPob];
+tempEv = [Ev,NewEv];
+
+[tempPob,ind] = unique(tempPob,'rows');
+tempEv = tempEv(1,ind);
+
+tempPob = [tempPob,tempEv'];
+tempPob = sortrows(temPob,length(tempPob),'descend');
+
+Pob_ = tempPob(1:size(Pob,1),1:size(tempPob,2)-1);
+Ev_ = tempEv(1:size(Pob,1));
+end
