8829aee046d1183f58a3e5fe651234452733cd4b
Rafa Roman <33625416+rafaroman18@users.noreply.github.com>
2018-12-05 19:07:33 +0100

    Add Pmut

--- Conjunto/MutarPoblacion_Satels.m
+++ Conjunto/MutarPoblacion_Satels.m
@@ -1,7 +1,9 @@
-function [newPobMutada] = MutarPoblacion_Satels(newPob)
+function [newPobMutada] = MutarPoblacion_Satels(newPob,Pmut)
     newPobMutada = zeros(size(newPob,1),size(newPob,2));
     i = 1;
     while i <= size(newPob,1)
+    if rand>=Pmut
        newPobMutada(i,:) = inversion(newPob(i,:));
+       end
     end
 end
