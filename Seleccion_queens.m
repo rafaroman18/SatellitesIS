719ebd374a1ec43effe0a9de80e52532afe1f756
BaronVonPollon <nearclex7@gmail.com>
2018-12-05 16:48:30 +0100

    Create Seleccion_queens.m

---
+++ Seleccion_queens.m
@@ -0,0 +1,18 @@
+function [Padres] = Seleccion_queens(FitPob,k)
+    Padres = zeros(1,k);
+    PUnit = 1 / sum(FitPob);
+    PrbAcum = cumsum(PUnit * FitPob);    
+    i = 1;
+    while i <= k
+        r = rand;
+        j = 1;
+        while PrbAcum(j) < r
+            j = j + 1;
+        end
+        c = ismember(j-1,Padres);
+        if c == 0
+            Padres(i) = j - 1;
+            i = i + 1;
+        end
+    end
+end
