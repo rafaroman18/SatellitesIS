0354037e39eaab151514b22de108d8a3b3086cc1
ikkani <urrutisanchez@hotmail.com>
2018-12-05 16:43:59 +0100

    dsadasdasd

---
+++ Iñaki/fEval.m
@@ -0,0 +1,14 @@
+function euclideaGlobal = fEval(rep, indRep, indTot)
+
+    for i=1: indTot
+        
+        if isempty(find(indRep == indTot(i)))
+           
+            
+            
+        end
+
+
+    end
+
+end
