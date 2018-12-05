2483fed1ee851ec5968556d1dd76ebb93414c5b3
Rafa Roman <33625416+rafaroman18@users.noreply.github.com>
2018-12-05 17:32:39 +0100

    Simple Crossover via Random Mask

---
+++ Conjunto/MaskCrossover.m
@@ -0,0 +1,22 @@
+function C = MaskCrossover(A,B)
+
+%We create the Mask
+%Mask = randi([0,1],1,size(A,2));
+Mask = [1 0 1 0 1 0 1];
+
+C = zeros(2,size(A,2));
+
+%First Son: 0 to the first parent, 1 to parent 2
+    m1 = find(Mask==0); %Positions where Mask is 0 .
+    m2 = find(Mask);    %Positions where Mask is 1.
+    C(1,m1) = A(m1);
+    C(1,m2) = B(m2);
+    
+%Second Son: 0 to the parent 2, 1 to the parent 1
+    m1 = find(Mask);      %Positions where Mask is 1.
+    m2 = find(Mask == 0); %Positions where Mask is 0.
+    C(2,m1) = A(m1);
+    C(2,m2) = B(m2);
+
+
+end
