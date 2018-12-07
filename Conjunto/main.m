a66c9fc5c8999ecb021fce2b43010303409edaab
ikkani <urrutisanchez@hotmail.com>
2018-12-07 20:49:41 +0100

    .

--- Conjunto/main.m
+++ Conjunto/main.m
@@ -5,17 +5,22 @@ addpath('./Genetic/');
 
 rand('seed',5);
 
-tenure      = 4;
-MAX_IT      = 1000;
-nSatelites  = 500;
-nRep        = 40;
-nPob        = 10;
-iteraciones = 10;
-
+tenure       = 4;
+MAX_IT       = 1000;
+nSatelites   = 500;
+nRep         = 40;
+nPob         = 10;
+iteraciones  = 100;
+tiemposAG = zeros(1,iteraciones);
+tiemposTaboo = zeros(1,iteraciones);
 
 
 for i = 1:iteraciones
+    tic
     [bestTaboo(i,:),fitBestTaboo(i)] = Taboo_Satels(nSatelites, nRep, MAX_IT, tenure);
-    [~ , fitBestAG]          = AG_Satels(nSatelites, nRep, nPob, MAX_IT);
-    fitBestAG(i) = min(fitBestAG);
+    tiemposTaboo(i) = toc;
+    tic
+    [~ , a]          = AG_Satels(nSatelites, nRep, nPob, MAX_IT);
+    tiemposAG(i) = toc;
+    fitBestAG(i) = min(a);
 end
