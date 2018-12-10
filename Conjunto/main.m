34e1dbeb8b320ff4e5e4501950ceee61eb98a50c
ikkani <urrutisanchez@hotmail.com>
2018-12-10 22:22:34 +0100

    main

--- Conjunto/main.m
+++ Conjunto/main.m
@@ -1,5 +1,4 @@
-function S = main()
-    % clc; clear all;
+    clc; clear all;
 
     addpath('./Taboo/');
     addpath('./Genetic/');
@@ -19,36 +18,51 @@ function S = main()
     fitBestAG = zeros(1,iteraciones);
     fitBestTab = zeros(1,iteraciones);
     
-    numItrsAG = zeros(1,iteraciones);
-    numItrsTab = zeros(1,iteraciones);
+    numItrsAG  = zeros(iteraciones,nPob,nSatelites);
+    numItrsTab = zeros(iteraciones,nSatelites);
 
+    tabooCell  = cell(2,3);
+    AGCell     = cell(10,3);
+    
+    
     for i = 1:iteraciones
+        for j=1:2
         tic
-        [NItsTab,FitTab] = Taboo_Satels(nSatelites, nRep, MAX_IT, tenure);
+        [NItsTab,FitTab] = Taboo_Satels(nSatelites, nRep, MAX_IT, j*2);
         tiemposTab(i) = toc;
         fitBestTab(i) = FitTab;
-        numItrsTab(i) = NItsTab;
-    
+        numItrsTab(i,:) = NItsTab;
+
+        tabooCell{j,1} = tiemposTab(i);
+        tabooCell{j,2} = fitBestTab(i);
+        tabooCell{j,3} = numItrsTab(i,:);
+        end
+        
+        for j=1:10
         tic
-        [NItsAG,FitAG] = AG_Satels(nSatelites, nRep, nPob, MAX_IT);
+        [NItsAG,FitAG] = AG_Satels(nSatelites, nRep, j*10, MAX_IT);
         tiemposAG(i) = toc;
         fitBestAG(i) = -max(FitAG);
-        numItrsAG(i) = NItsAG;
+        numItrsAG(i,:,:) = NItsAG;
+        
+        AGCell{j,1} = tiemposAG(i);
+        AGCell{j,2} = -max(FitAG);
+        AGCell{j,3} = NItsAG;
+        end
     end
     
-    disp('BÚSQUEDA TABOO');
-    x1 = ['Tiempos:     ',num2str(tiemposTab)];
-    disp(x1);
-    x2 = ['Valoracs.:   ',num2str(fitBestTab)];
-    disp(x2);
-    x3 = ['Num. Iters.: ',num2str(numItrsTab)];
-    disp(x3);
-    
-    disp('ALGORITMO GENÉTICO');
-    x1 = ['Tiempos:     ',num2str(tiemposAG)];
-    disp(x1);
-    x2 = ['Valoracs.:   ',num2str(fitBestAG)];
-    disp(x2);
-    x3 = ['Num. Iters.: ',num2str(numItrsAG)];
-    disp(x3);
-end
+%     disp('BÚSQUEDA TABOO');
+%     x1 = ['Tiempos:     ',num2str(tiemposTab)];
+%     disp(x1);
+%     x2 = ['Valoracs.:   ',num2str(fitBestTab)];
+%     disp(x2);
+%     x3 = ['Num. Iters.: ',num2str(numItrsTab)];
+%     disp(x3);
+%     
+%     disp('ALGORITMO GENÉTICO');
+%     x1 = ['Tiempos:     ',num2str(tiemposAG)];
+%     disp(x1);
+%     x2 = ['Valoracs.:   ',num2str(fitBestAG)];
+%     disp(x2);
+%     x3 = ['Num. Iters.: ',num2str(numItrsAG)];
+%     disp(x3);
