85410cfd90b9fb1cc568ae8490256212648b780a
BaronVonPollon <nearclex7@gmail.com>
2018-12-10 15:49:50 +0100

    Add files via upload
    Update: Prints the results

--- Conjunto/main.m
+++ Conjunto/main.m
@@ -1,26 +1,54 @@
-clc; clear all;
+function S = main()
+    % clc; clear all;
 
-addpath('./Taboo/');
-addpath('./Genetic/');
+    addpath('./Taboo/');
+    addpath('./Genetic/');
 
-rand('seed',5);
+    rand('seed',5);
 
-tenure       = 4;
-MAX_IT       = 1000;
-nSatelites   = 500;
-nRep         = 40;
-nPob         = 10;
-iteraciones  = 100;
-tiemposAG = zeros(1,iteraciones);
-tiemposTaboo = zeros(1,iteraciones);
+    tenure       = 4;
+    MAX_IT       = 1000;
+    nSatelites   = 500;
+    nRep         = 40;
+    nPob         = 50;
+    iteraciones  = 10;
 
+    tiemposAG = zeros(1,iteraciones);
+    tiemposTab = zeros(1,iteraciones);
 
-for i = 1:iteraciones
-    tic
-    [bestTaboo(i,:),fitBestTaboo(i)] = Taboo_Satels(nSatelites, nRep, MAX_IT, tenure);
-    tiemposTaboo(i) = toc;
-    tic
-    [~ , a]          = AG_Satels(nSatelites, nRep, nPob, MAX_IT);
-    tiemposAG(i) = toc;
-    fitBestAG(i) = min(a);
+    fitBestAG = zeros(1,iteraciones);
+    fitBestTab = zeros(1,iteraciones);
+    
+    numItrsAG = zeros(1,iteraciones);
+    numItrsTab = zeros(1,iteraciones);
+
+    for i = 1:iteraciones
+        tic
+        [NItsTab,FitTab] = Taboo_Satels(nSatelites, nRep, MAX_IT, tenure);
+        tiemposTab(i) = toc;
+        fitBestTab(i) = FitTab;
+        numItrsTab(i) = NItsTab;
+    
+        tic
+        [NItsAG,FitAG] = AG_Satels(nSatelites, nRep, nPob, MAX_IT);
+        tiemposAG(i) = toc;
+        fitBestAG(i) = -min(FitAG);
+        numItrsAG(i) = NItsAG;
+    end
+    
+    disp('BÚSQUEDA TABOO');
+    x1 = ['Tiempos:     ',num2str(tiemposTab)];
+    disp(x1);
+    x2 = ['Valoracs.:   ',num2str(fitBestTab)];
+    disp(x2);
+    x3 = ['Num. Iters.: ',num2str(numItrsTab)];
+    disp(x3);
+    
+    disp('ALGORITMO GENÉTICO');
+    x1 = ['Tiempos:     ',num2str(tiemposAG)];
+    disp(x1);
+    x2 = ['Valoracs.:   ',num2str(fitBestAG)];
+    disp(x2);
+    x3 = ['Num. Iters.: ',num2str(numItrsAG)];
+    disp(x3);
 end
