44bdcbbdbe9ce6aac8ca38fd2c9e663dac1eb2af
ikkani <urrutisanchez@hotmail.com>
2018-12-13 12:25:23 +0100

    main

--- Conjunto/main.m
+++ Conjunto/main.m
@@ -33,22 +33,22 @@
         fitBestTab(i) = FitTab;
         numItrsTab(i,:) = NItsTab;
 
-        tabooCell{j,1,i} = tiemposTab(i);
-        tabooCell{j,2,i} = fitBestTab(i);
-        tabooCell{j,3,i} = numItrsTab(i,:);
+%        tabooCell{j,1,i} = tiemposTab(i);
+%        tabooCell{j,2,i} = fitBestTab(i);
+%        tabooCell{j,3,i} = numItrsTab(i,:);
         end
         
-%         for j=1:10
-%         tic
-%         [NItsAG,FitAG] = AG_Satels(nSatelites, nRep, j*10, MAX_IT);
-%         tiemposAG(i) = toc;
-%         fitBestAG(i) = -max(FitAG);
-%         %numItrsAG(i,:,:) = NItsAG;
-%         
+        for j=1:10
+        tic
+        [NItsAG,FitAG] = AG_Satels(nSatelites, nRep, j*10, MAX_IT);
+        tiemposAG(i) = toc;
+        fitBestAG(i) = -max(FitAG);
+        numItrsAG(i,:,:) = NItsAG;
+         
 %         AGCell{j,1,i} = tiemposAG(i);
 %         AGCell{j,2,i} = -max(FitAG);
 %         AGCell{j,3,i} = NItsAG;
-%         end
+        end
     end
     
 %     disp('BÚSQUEDA TABOO');
